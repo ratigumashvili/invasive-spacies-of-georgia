2045d76d85785efaa50be8904b218cf02cd9cd81
ratigumashvili <ratigumashvili@gmail.com>
2025-03-03 23:13:29 +0400

    cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
 import { NextResponse } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+
+import { routing } from './i18n/routing';
 
 const intlMiddleware = createMiddleware(routing);
 
