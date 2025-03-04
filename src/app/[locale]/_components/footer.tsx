63511bef500a1ca6c3deeb86818cf06c83e9c29e
ratigumashvili <ratigumashvili@gmail.com>
2025-03-04 18:57:13 +0400

    finished footer

--- src/app/[locale]/_components/footer.tsx
+++ src/app/[locale]/_components/footer.tsx
@@ -12,8 +12,8 @@ export function Footer() {
     const t = useTranslations("Footer")
     return (
         <footer className="mt-auto bg-black text-white shadow-gray-300">
-            <div className="w-full max-w-7xl mx-auto py-8 px-4 sm:px-8 flex items-center justify-between">
-                <div className="text-sm flex flex-col gap-y-1">
+            <div className="w-full max-w-7xl mx-auto py-8 px-4 sm:px-8 flex flex-col md:flex-row items-center justify-between">
+                <div className="text-sm flex flex-col text-center md:text-left gap-y-1 mb-4 md:mb-0 px-4 md:px-0">
                     <p className="flex items-center gap-1">
                     <CopyrightIcon className="w-3 h-3" /> {detectDate()}, {t("isu_full")}. {t("zoology")}
                     </p>
