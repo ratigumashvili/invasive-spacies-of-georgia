d8f7d24c5ce29662773d2b54dbb946b8630dba97
ratigumashvili <ratigumashvili@gmail.com>
2025-03-10 10:39:53 +0400

    added custom button to map

--- src/app/[locale]/_components/home-page-map/home-page-map.tsx
+++ src/app/[locale]/_components/home-page-map/home-page-map.tsx
@@ -3,11 +3,10 @@
 import { useEffect, useState } from 'react';
 import { Link } from '@/i18n/routing';
 import { Icon, LatLngExpression } from 'leaflet'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import { useTranslations } from 'next-intl';
-
-
 
 import { useLocation } from '@/hooks/use-location';
 
@@ -17,6 +16,42 @@ const customIcon = new Icon({
     iconUrl: "/pin.png",
     iconSize: [28, 28]
 })
+
+const CustomButton = () => {
+    const map = useMap();
+
+    useEffect(() => {
+        const customControl = new L.Control({ position: "topleft" });
+
+        customControl.onAdd = () => {
+            const container = L.DomUtil.create("div", "leaflet-bar leaflet-control");
+
+            const button = L.DomUtil.create("button", "", container);
+            button.innerHTML = `&quest;`;
+            button.style.fontWeight = "bold";
+            button.style.fontSize = "16px";
+            button.style.backgroundColor = "white";
+            button.style.padding = "2px 9px";
+            button.style.cursor = "pointer";
+            button.style.border = "1px solid #ccc";
+            button.style.borderRadius = "4px";
+
+            button.onclick = () => {
+                console.log("clicked")
+            };
+
+            return container;
+        };
+
+        customControl.addTo(map);
+
+        return () => {
+            customControl.remove();
+        };
+    }, [map]);
+
+    return null;
+};
 
 export default function HomePageMap({ data }: { data: [number, number][] }) {
 
@@ -56,6 +91,8 @@ export default function HomePageMap({ data }: { data: [number, number][] }) {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
+                <CustomButton />
+
                 <MarkerClusterGroup>
                     {data.map((marker, index) => (
                         <Marker
@@ -69,7 +106,7 @@ export default function HomePageMap({ data }: { data: [number, number][] }) {
                                 {markerAddresses[`${marker[0]},${marker[1]}`] && (
                                     <Link
                                         href={`/search?coordinates=${marker}`}
-                                        className="block my-2 !text-red-700 !hover:text-red-600 transition"
+                                        className="block my-2 !text-sky-800 !hover:text-red-600 transition"
                                     >
                                         {t("readMore")}
                                     </Link>
