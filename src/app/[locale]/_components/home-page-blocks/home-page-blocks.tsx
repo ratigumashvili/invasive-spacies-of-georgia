83f3aead3720a64a6d3644c397cd911d6f7e48c3
ratigumashvili <ratigumashvili@gmail.com>
2025-03-11 21:24:59 +0400

    refactored events

--- src/app/[locale]/_components/home-page-blocks/home-page-blocks.tsx
+++ src/app/[locale]/_components/home-page-blocks/home-page-blocks.tsx
@@ -1,7 +1,15 @@
-import { EventBlock } from "./event-block";
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { EventBlock } from "@/app/[locale]/_components/home-page-blocks/event-block";
+
 import { HomePageBlocksProps } from "@/types/home-page-blocks";
 
 export async function HomePageBlocks({ events }: HomePageBlocksProps) {
+
+    const t = useTranslations("Common")
 
     return (
         <div className="flex flex-col md:flex-row gap-4 mb-8">
@@ -12,7 +20,22 @@ export async function HomePageBlocks({ events }: HomePageBlocksProps) {
                 <h2 className="text-xl">Species Factsheets</h2>
             </div>
             <div className="flex flex-col gap-2 flex-1">
-                <EventBlock events={events} />
+                <h2 className="text-xl font-medium mb-3">Events</h2>
+                <Card className="rounded-none bg-slate-50">
+                    <CardHeader className="sr-only">
+                        <CardTitle className="text-xl">
+                            {t("upcomming_events")}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <EventBlock events={events} />
+                    </CardContent>
+                    <CardFooter>
+                        <Button asChild size="lg" variant="default" className="w-full sm:w-max">
+                            <Link href={"/events"}>{t("see_all_events")}</Link>
+                        </Button>
+                    </CardFooter>
+                </Card>
             </div>
         </div>
     )
--- src/app/[locale]/events/page.tsx
+++ src/app/[locale]/events/page.tsx
@@ -1,17 +1,29 @@
+import { useTranslations } from "next-intl";
+
 import Container from "@/app/[locale]/_components/container";
+import { EventBlock } from "@/app/[locale]/_components/home-page-blocks/event-block";
 
 import { getEvents } from "@/lib/api-calls";
+import { generateFontByLocale } from "@/lib/utils";
 
-export default async function EvensPage({params}: {params: {locale: string}}) {
-    const {locale} = await params;
+const PageTitle = ({ locale }: { locale: string }) => {
+    const t = useTranslations("Common")
+    return (
+        <h1 className={`${generateFontByLocale(locale)} text-2xl font-medium mb-8`}>
+            {t("upcomming_events")}
+        </h1>
+    )
+}
+
+export default async function EvensPage({ params }: { params: { locale: string } }) {
+    const { locale } = await params;
 
     const events = await getEvents(locale)
 
     return (
         <Container>
-            <pre>
-                {JSON.stringify(events, null, 3)}
-            </pre>
+            <PageTitle locale={locale} />
+            <EventBlock events={events} />
         </Container>
     )
 }
