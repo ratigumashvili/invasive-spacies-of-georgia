d28ac301b7fde8ea1c6a30e330b33372ab757b54
ratigumashvili <ratigumashvili@gmail.com>
2025-03-11 11:33:54 +0400

    working on calendar

--- src/app/[locale]/_components/home-page-blocks.tsx
+++ src/app/[locale]/_components/home-page-blocks.tsx
@@ -11,7 +11,9 @@ export async function HomePageBlocks() {
             </div>
             <div className="flex flex-col gap-2 flex-1">
                 <h2 className="text-xl">Events</h2>
-                <CalendarItem start="25" end="28" month="mar" year="2025" />
+                <CalendarItem id="1" start="25" end="28" startMonth="mar" year="2025" title="EMCA Conference 2025" location="Antwerp, Belgium" />
+                <CalendarItem id="2" start="14" end="17" startMonth="apr" year="2025" title="National Forum on Biological Control Conference" location="Maryland, USA" />
+                <CalendarItem id="3" start="21" end="24" startMonth="sep" year="2025" title="5th International Congress on Biological Invasions" location="Perth, Australia" />
             </div>
         </div>
     )
