f5cdfda8d4e8b4ec80ca60d0cbbcc03a2ead65f8
ratigumashvili <ratigumashvili@gmail.com>
2025-03-12 23:40:22 +0400

    added basic nothing-found component

--- src/app/[locale]/_components/nothing-found.tsx
+++ src/app/[locale]/_components/nothing-found.tsx
@@ -1,9 +1,14 @@
 import Container from "@/app/[locale]/_components/container";
+import { useTranslations } from "next-intl";
 
-export function NothingFound () {
+export function NothingFound ({text}: {text?: string}) {
+
+    const t = useTranslations("Common")
+
     return (
         <Container>
-            <h1 className="text-2xl font-medium">Nothing found</h1>
+            <h1 className="text-2xl font-medium">{t("nothing_found")}</h1>
+            <h2 className="text-xl">{text}</h2>
         </Container>
     )
 }
