a2e9284b73e00eaeed6aa81f6de4ced6d0360e44
ratigumashvili <ratigumashvili@gmail.com>
2025-03-12 19:08:02 +0400

    small fix

--- src/app/[locale]/_components/footer.tsx
+++ src/app/[locale]/_components/footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@/i18n/routing"
 import { CopyrightIcon } from "lucide-react"
 import { useTranslations } from "next-intl"
 import Image from "next/image"
@@ -17,7 +18,8 @@ export function Footer() {
                     <p className="flex items-center gap-1">
                     <CopyrightIcon className="w-3 h-3" /> {detectDate()}, {t("isu_full")}. {t("zoology")}
                     </p>
-                    <p className="text-xs">{t("address")}</p>
+                    <p className="text-xs">{t("address")}. <Link href={`${t("url")}`}>{t("website")}</Link></p>
+                    <p className="text-xs my-2">Developed @<Link href={"mailto:dh@iliauni.edu.ge"}>DH ISU</Link></p>
                 </div>
                 <Image 
                     src={"/iliauni-logo_eng.png"}
