12d0e5e8d301a27422ec6c118a75db30a03f980f
ratigumashvili <ratigumashvili@gmail.com>
2025-03-14 08:41:49 +0400

    added basic meta

--- src/app/[locale]/_components/single-taxon-meta.tsx
+++ src/app/[locale]/_components/single-taxon-meta.tsx
@@ -11,22 +11,40 @@ export function SingleTaxonMeta({ data }: SingleTaxonMetaProps) {
     const t = useTranslations("Species")
     return (
         <div className="border border-sky-800 border-l-8 bg-slate-50 my-8 p-4">
-            <h2 className="text-xl font-medium mb-4">{data[0]?.name}</h2>
+            <h2 className="text-2xl font-medium mb-4">{data[0]?.name}</h2>
+
+            <h3 className="text-lg font-medium my-4 text-muted-foreground">{t("metadata")}</h3>
+
             <dl className="data-list">
-                <dt>{t("scientific_name_id")}</dt>
+                <dt>{t("scientific_name")}</dt>
                 <dd>{data[0]?.name}</dd>
+                <dt>{t("name_id")}</dt>
+                <dd>456</dd>
                 <dt>{t("nat")}</dt>
                 <dd>{data[0]?.autorName}</dd>
-                <dd></dd>
-                <dt></dt>
-                <dd></dd>
-                <dt></dt>
-                <dd></dd>
-                <dt></dt>
-                <dd></dd>
-                <dt></dt>
-                <dd></dd>
-                <dt></dt>
+                <dt>{t("eco_group")}</dt>
+                <dd>{data[0]?.ecologicalGroup}</dd>
+                <dt>{t("status")}</dt>
+                <dd>Alien</dd>
+                <dt>{t("enviroments")}</dt>
+                <dd>Freshwater, Terrestrial</dd>
+                <dt>{t("concern")}</dt>
+                <dd>Yes</dd>
+                <dt>{t("partly_native")}</dt>
+                <dd>Yes</dd>
+                <dt>{t("horizon_scaning")}</dt>
+                <dd>No</dd>
+                <dt>{t("impact")}</dt>
+                <dd>Yes</dd>
+                <dt>{t("impact_source")}</dt>
+                <dd>CABI NOBANIS SEBI</dd>
+            </dl>
+
+            <h3 className="text-lg font-medium my-4 text-muted-foreground">{t("taxonomy")}</h3>
+
+            <dl className="data-list">
+                <dt>{t("kingdom")}</dt>
+                <dd>{data[0]?.kingdom.name}</dd>
             </dl>
         </div>
     )
