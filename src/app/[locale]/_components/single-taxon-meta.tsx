0aa42f40bb96fe0883285f1550820e65297002a1
ratigumashvili <ratigumashvili@gmail.com>
2025-03-14 08:02:22 +0400

    added species translation

--- src/app/[locale]/_components/single-taxon-meta.tsx
+++ src/app/[locale]/_components/single-taxon-meta.tsx
@@ -6,10 +6,16 @@ type SingleTaxonMetaProps = {
     data: SpeciesEntity[];
 };
 
-export function SingleTaxonMeta ({data}: SingleTaxonMetaProps) {
+export function SingleTaxonMeta({ data }: SingleTaxonMetaProps) {
     return (
         <div className="border border-sky-800 border-l-8 bg-slate-50 my-8 p-4">
-            <h2 className="text-xl font-medium">{data[0]?.name}</h2>
+            <h2 className="text-xl font-medium mb-4">{data[0]?.name}</h2>
+            <dl className="data-list">
+                <dt>Coffee</dt>
+                <dd>Black hot drink</dd>
+                <dt>Milk</dt>
+                <dd>White cold drink</dd>
+            </dl>
         </div>
     )
 }
