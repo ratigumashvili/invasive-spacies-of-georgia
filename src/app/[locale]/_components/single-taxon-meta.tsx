676655b6017ef18f6870a1f47454d87faa09ca1d
ratigumashvili <ratigumashvili@gmail.com>
2025-03-14 08:04:59 +0400

    working on singleTaxonMeta

--- src/app/[locale]/_components/single-taxon-meta.tsx
+++ src/app/[locale]/_components/single-taxon-meta.tsx
@@ -1,20 +1,32 @@
 "use client"
 
 import { SpeciesEntity } from "@/types/taxonomy-types";
+import { useTranslations } from "next-intl";
 
 type SingleTaxonMetaProps = {
     data: SpeciesEntity[];
 };
 
 export function SingleTaxonMeta({ data }: SingleTaxonMetaProps) {
+    const t = useTranslations("Species")
     return (
         <div className="border border-sky-800 border-l-8 bg-slate-50 my-8 p-4">
             <h2 className="text-xl font-medium mb-4">{data[0]?.name}</h2>
             <dl className="data-list">
-                <dt>Coffee</dt>
-                <dd>Black hot drink</dd>
-                <dt>Milk</dt>
-                <dd>White cold drink</dd>
+                <dt>{t("scientific_name_id")}</dt>
+                <dd>{data[0]?.name}</dd>
+                <dt>{t("nat")}</dt>
+                <dd>{data[0]?.autorName}</dd>
+                <dd></dd>
+                <dt></dt>
+                <dd></dd>
+                <dt></dt>
+                <dd></dd>
+                <dt></dt>
+                <dd></dd>
+                <dt></dt>
+                <dd></dd>
+                <dt></dt>
             </dl>
         </div>
     )
