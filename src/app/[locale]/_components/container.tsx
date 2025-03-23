c9d5181da86cd78863d8b5dc5a3c246e5602c92c
ratigumashvili <ratigumashvili@gmail.com>
2025-03-23 11:23:36 +0400

    small fix

--- src/app/[locale]/_components/container.tsx
+++ src/app/[locale]/_components/container.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export default function Container({children, className}: {children: React.ReactNode, className?: string}) {
     return (
-        <section className={cn("w-full max-w-7xl mx-auto px-4 sm:px-8 py-8", className)}>
+        <section className={cn("w-full max-w-7xl mx-auto px-4 sm:px-8 py-8 mb-8", className)}>
             {children}
         </section>
     )
