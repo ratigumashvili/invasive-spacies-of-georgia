bd543c9ab7b447743cb12d41a551c2763e7ccabf
ratigumashvili <ratigumashvili@gmail.com>
2025-03-24 08:20:06 +0400

    small fix

--- src/app/[locale]/_components/places/single-place.tsx
+++ src/app/[locale]/_components/places/single-place.tsx
@@ -85,7 +85,7 @@ export function SinglePlaceComponent({
     return (
         <div>
             <pre>
-                {}
+                { }
             </pre>
             <div className="mb-8 flex items-start justify-baseline">
                 <div className="w-full">
@@ -113,34 +113,36 @@ export function SinglePlaceComponent({
                 </div>
             ) : (
                 <div className="w-full overflow-x-auto">
-                    <table className="w-full min-w-7xl">
-                        <thead>
-                            <tr>
-                                <th className="text-nowrap">{t("scientific_name")}</th>
-                                <th className="text-nowrap">{t("nat")}</th>
-                                <th className="text-nowrap">{t("eco_group")}</th>
-                                <th>{t("family")}</th>
-                                <th>{t("genus")}</th>
-                                <th className="text-nowrap">{t("first_introduced")}</th>
-                                <th className="text-nowrap">{t("detected")}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data?.map((item) => (
-                                <tr key={item.documentId}>
-                                    <td>
-                                        <Link href={`/species-list/${item.slug}`} className="text-sky-800 font-medium hover:underline">{item.name}</Link>
-                                    </td>
-                                    <td>{item.autorName}</td>
-                                    <td>{item.ecologicalGroup}</td>
-                                    <td>{item.family?.name}</td>
-                                    <td>{item.genus?.name}</td>
-                                    <td>{item.firstIntroduced}</td>
-                                    <td>{item.dateOfDetection}</td>
+                    {data?.length !== 0 && (
+                        <table className="w-full min-w-7xl">
+                            <thead>
+                                <tr>
+                                    <th className="text-nowrap">{t("scientific_name")}</th>
+                                    <th className="text-nowrap">{t("nat")}</th>
+                                    <th className="text-nowrap">{t("eco_group")}</th>
+                                    <th>{t("family")}</th>
+                                    <th>{t("genus")}</th>
+                                    <th className="text-nowrap">{t("first_introduced")}</th>
+                                    <th className="text-nowrap">{t("detected")}</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {data?.map((item) => (
+                                    <tr key={item.documentId}>
+                                        <td>
+                                            <Link href={`/species-list/${item.slug}`} className="text-sky-800 font-medium hover:underline">{item.name}</Link>
+                                        </td>
+                                        <td>{item.autorName}</td>
+                                        <td>{item.ecologicalGroup}</td>
+                                        <td>{item.family?.name}</td>
+                                        <td>{item.genus?.name}</td>
+                                        <td>{item.firstIntroduced}</td>
+                                        <td>{item.dateOfDetection}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
                 </div>
             )}
         </div>
