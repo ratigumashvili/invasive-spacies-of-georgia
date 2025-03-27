6d2ea85bd8239ce6eaebe9c0bbef7aef0ca2b5e3
ratigumashvili <ratigumashvili@gmail.com>
2025-03-27 08:26:50 +0400

    finished ssc

--- src/app/[locale]/_components/single-specie-cite.tsx
+++ src/app/[locale]/_components/single-specie-cite.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { useRef } from "react"
 import { format } from "date-fns"
+import { toast } from "sonner"
+import { ClipboardCopyIcon } from "lucide-react"
 
 import { useFullUrl } from "@/hooks/use-full-url"
 import { formatNameToCitationStyle, separator } from "@/lib/utils"
@@ -25,13 +28,23 @@ export function SingleSpecieCite({
     appVersion,
 }: SingleSpecieCiteProps) {
 
+    const ref = useRef<HTMLSpanElement | null>(null)
     const url = useFullUrl()
 
+    function handleCopy () {
+        navigator.clipboard.writeText(ref?.current?.innerText as string);
+        toast.success("Citation copied to clipboard")
+    }
+
     return (
-        <div className="p-4 text-sm mt-4 border">
-            {authors.map((author, index) => (
-                <span key={author.id}>{formatNameToCitationStyle(author.username)}{separator(index, authors, ", ", " ")}</span>
-            ))} 2005. <em>{recordTitle}</em>, {recordAuthor} – FactSheet. {appTitle}, {appSubTitle} {appVersion}. Accessed at: {url}. Date accessed: {format(new Date(Date.now()), "PPP")}
+        <div className="p-4 text-sm mt-4 border relative pr-[60px]">
+            <span className="font-medium pr-1">Cite:</span>
+            <span ref={ref}>
+                {authors.map((author, index) => (
+                    <span key={author.id}>{formatNameToCitationStyle(author.username)}{separator(index, authors, ", ", " ")}</span>
+                ))} 2005. <em>{recordTitle}</em>, {recordAuthor} – FactSheet. {appTitle}, {appSubTitle} {appVersion}. Accessed at: {url}. Date accessed: {format(new Date(Date.now()), "PPP")}
+            </span>
+            <ClipboardCopyIcon className="w-4 h-4 absolute right-4 top-4 cursor-pointer" onClick={handleCopy} />
         </div>
     )
 }
