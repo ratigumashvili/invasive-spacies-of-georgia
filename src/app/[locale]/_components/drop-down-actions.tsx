0bff444ac6d9e843254356fa11bbd63b44d9f721
ratigumashvili <ratigumashvili@gmail.com>
2025-03-27 07:07:33 +0400

    added copyToClipboard function

--- src/app/[locale]/_components/drop-down-actions.tsx
+++ src/app/[locale]/_components/drop-down-actions.tsx
@@ -6,9 +6,11 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useFullUrl } from "@/hooks/use-full-url"
 import { exportDataAsCSV } from "@/lib/utils"
 import { DistributionDownload, SpecieDownload } from "@/types/specie-response"
 import { SettingsIcon } from "lucide-react"
+import { toast } from "sonner"
 
 export function DropDownAction({
     specieData, 
@@ -17,6 +19,14 @@ export function DropDownAction({
     specieData: SpecieDownload, 
     distribution: DistributionDownload[]
 }) {
+
+    const url = useFullUrl()
+
+    function copyToClipboard () {
+        navigator.clipboard.writeText(url)
+        toast.success("Link copied to clipboard")
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -25,7 +35,9 @@ export function DropDownAction({
                 </button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="bg-white z-10 min-w-[200px] rounded-none" align="end">
-                <DropdownMenuItem className="text-base cursor-pointer rounded-none">Copy url</DropdownMenuItem>
+                <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={copyToClipboard}>
+                    Copy url
+                </DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none">Print page</DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={() => exportDataAsCSV(specieData)}>
                     Download specie data
