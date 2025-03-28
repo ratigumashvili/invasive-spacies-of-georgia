96a116b38e928244cc5daba92441ef0bacd6a096
ratigumashvili <ratigumashvili@gmail.com>
2025-03-28 21:27:50 +0400

    added detectLifeForm function

--- src/app/[locale]/_components/species-table.tsx
+++ src/app/[locale]/_components/species-table.tsx
@@ -5,6 +5,20 @@ import { Species } from "@/types/specie-response"
 
 export function SpeciesTable({ data }: { data: Species[] }) {
     const t = useTranslations("Species")
+
+    function detectLifeForm(value: string) {
+        switch (value) {
+            case "aquatic":
+                return t("aquatic")
+            case "semiaquatic":
+                return t("semiaquatic")
+            case "terrestrial":
+                return t("terrestrial")
+            default:
+                return "Unknown";
+        }
+    }
+
     return (
         <div className="w-full overflow-x-auto">
             {data?.length !== 0 && (
@@ -27,7 +41,7 @@ export function SpeciesTable({ data }: { data: Species[] }) {
                                     <Link href={`/species-list/${item.slug}`} className="text-sky-800 font-medium hover:underline">{item.name}</Link>
                                 </td>
                                 <td>{item.autorName}</td>
-                                <td>{item.lifeForm}</td>
+                                <td>{detectLifeForm(item.lifeForm as string)}</td>
                                 <td>{item.family?.name}</td>
                                 <td>{item.genus?.name}</td>
                                 <td>{item.firstRecorded}</td>
