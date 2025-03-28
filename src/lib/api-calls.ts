3e50b1f7bf7cb13bc44b674f2a6af0ac5dfe57e9
ratigumashvili <ratigumashvili@gmail.com>
2025-03-28 12:15:29 +0400

    added fields to specie search populate

--- src/lib/api-calls.ts
+++ src/lib/api-calls.ts
@@ -664,7 +664,15 @@ export async function searchSpecieByType(locale: string, type: string, name: str
     };
 
     const queryParams = {
-      fields: ["name", "slug"],
+      fields: ["name", "slug", "autorName", "lifeForm", "firstRecorded", "dateOfDetection"],
+      populate: {
+        family: {
+          fields: ["name", "slug"]
+        },
+        genus: {
+          fields: ["name", "slug"]
+        }
+      },
       locale,
       filters: {
         $and: [dynamicFilter],
@@ -703,7 +711,15 @@ export async function searchSpecieByType(locale: string, type: string, name: str
 export async function searchSpecieByName(locale: string, name: string, page: number = 1, pageSize: number = 1) {
   try {
     const queryParams = {
-      fields: ["name", "slug"],
+      fields: ["name", "slug", "autorName", "lifeForm", "firstRecorded", "dateOfDetection"],
+      populate: {
+        family: {
+          fields: ["name", "slug"]
+        },
+        genus: {
+          fields: ["name", "slug"]
+        }
+      },
       locale,
       filters: {
         $and: [
