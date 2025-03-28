fa46ca0d7e1602b20316b8fbac3041b0e2a2b230
ratigumashvili <ratigumashvili@gmail.com>
2025-03-28 06:48:25 +0400

    started working on search

--- src/app/[locale]/_components/search.tsx
+++ src/app/[locale]/_components/search.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { useSearchParams } from "next/navigation"
 import { useTranslations } from "next-intl"
+import { useRouter } from "@/i18n/routing"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -25,20 +27,31 @@ const taxonomy = [
 
 export function SearchComponent() {
     const t = useTranslations("Search")
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const defaultValue = searchParams.get("type") || "specie"
+
+    function handleFormSubmit(formData: FormData) {
+        const name = formData.get("name");
+        const type = formData.get("type")
+
+        router.push(`/search?name=${name}&type=${type}`)
+    }
+
     return (
         <Card className="rounded-none bg-slate-50">
             <CardHeader>
                 <CardTitle className="text-xl">{t("search_for_species")}</CardTitle>
             </CardHeader>
             <CardContent>
-                <form>
+                <form action={handleFormSubmit}>
                     <div className="flex items-center gap-2 mb-4">
                         <Input id="name" name="name" className="py-4 px-3 rounded-none bg-white" placeholder={t("placeholder_text")} />
                         <Button type="button" variant="destructive" className="rounded-none cursor-pointer">{t("cancel")}</Button>
-                        <Button type="button" className="rounded-none cursor-pointer">{t("submit")}</Button>
+                        <Button type="submit" className="rounded-none cursor-pointer">{t("submit")}</Button>
                     </div>
                     <div>
-                        <RadioGroup defaultValue="specie" className="flex items-center gap-2">
+                        <RadioGroup defaultValue={defaultValue} name="type" className="flex items-center gap-2">
                             {taxonomy.map((item) => (
                                 <div key={item.label} className="flex gap-x-2">
                                     <RadioGroupItem value={item.label} id={item.label} className="bg-white" />
