b32359a338170ce83b31c57708a642a9aa3ae948
ratigumashvili <ratigumashvili@gmail.com>
2025-03-29 10:25:09 +0400

    added page title

--- src/app/[locale]/research/page.tsx
+++ src/app/[locale]/research/page.tsx
@@ -1,12 +1,24 @@
-import Container from "@/app/[locale]/_components/container";
-import { Pagination } from "@/app/[locale]/_components/pagination";
+import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 
+import Container from "@/app/[locale]/_components/container";
+import { Pagination } from "@/app/[locale]/_components/pagination";
+
 import { fetchResearches } from "@/lib/api-calls";
+import { generateFontByLocale } from "@/lib/utils";
 
 type Props = {
     params: Promise<{ locale: string }>
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+const PageTitle = ({ locale }: { locale: string }) => {
+    const t = useTranslations("Research")
+    return (
+        <h1 className={`${generateFontByLocale(locale)} text-2xl uppercase font-medium mb-8`}>
+            {t("page_title")}
+        </h1>
+    )
 }
 
 export default async function Research({ params, searchParams }: Props) {
@@ -20,6 +32,8 @@ export default async function Research({ params, searchParams }: Props) {
 
     return (
         <Container>
+
+            <PageTitle locale={locale} />
 
             <ul>
                 {response?.data?.map((research) => (
