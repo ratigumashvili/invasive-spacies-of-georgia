5c7944b7578ea364b555c6b4cb66b1555dd8ff15
ratigumashvili <ratigumashvili@gmail.com>
2025-03-30 11:47:24 +0400

    added basic query to fetch legal-docs

--- src/app/[locale]/legal-docs/page.tsx
+++ src/app/[locale]/legal-docs/page.tsx
@@ -1,7 +1,43 @@
+import qs from "qs";
 import Container from "@/app/[locale]/_components/container";
+import { getSinglePage } from "@/lib/api-calls";
 
-export default function LegalDocs() {
+type Props = {
+    params: Promise<{ locale: string }>
+}
+
+interface LegalDocumetns {
+    id: number,
+    documentId: string
+}
+
+const filterQuery = {
+    populate: {
+        records: {
+            fields: ["id", "title", "url", "description"],
+            populate: {
+                date: {
+                    fields: ["day", "month", "year"]
+                }
+            }
+        }
+    }
+}
+
+const query = qs.stringify(filterQuery, {encodeValuesOnly: true})
+
+export default async function LegalDocs({ params }: Props) {
+    const { locale } = await params
+
+    const fetcLegalDocs = async (locale: string): Promise<LegalDocumetns> => {
+        return await getSinglePage<LegalDocumetns>("legal-document", locale, query);
+    };
+    
+    const response = await fetcLegalDocs(locale)
+
     return (
-        <Container>Legal docs</Container>
+        <Container>Legal docs
+            <pre>{JSON.stringify(response, null, 2)}</pre>
+        </Container>
     )
 }
