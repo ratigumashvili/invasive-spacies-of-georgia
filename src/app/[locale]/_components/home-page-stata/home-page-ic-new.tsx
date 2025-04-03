a6f5b203d8922056ae9976c46ffa507189deae19
ratigumashvili <ratigumashvili@gmail.com>
2025-04-03 11:26:15 +0400

    refactored stata

--- src/app/[locale]/_components/home-page-stata/home-page-ic-new.tsx
+++ src/app/[locale]/_components/home-page-stata/home-page-ic-new.tsx
@@ -16,24 +16,24 @@ export function HomePageInfoCardNew({
     const t = useTranslations("Common")
     
     return (
-        <div className="bg-sky-950 text-white grid grid-cols-1 md:grid-cols-3 mb-8 p-2 rounded-md">
-            <div className="col-span-1 border-r-none md:border-r md:border-r-white/50 p-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 mb-8 gap-6">
+            <div className="col-span-1 p-6 bg-white rounded-md text-sky-950">
                 <h2 className={cn("uppercase text-xl", generateFontByLocale(locale))}>
                     {t("total")}
                 </h2>
-                <p className="text-5xl">{totalRecords}</p>
+                <p className="text-5xl font-medium">{totalRecords}</p>
             </div>
-            <div className="col-span-1 border-r-none md:border-r md:border-r-white/50 p-6">
+            <div className="col-span-1 p-6 bg-white rounded-md text-sky-950">
                 <h2 className={cn("uppercase text-xl", generateFontByLocale(locale))}>
                     {t("contributed")}
                 </h2>
-                <p className="text-5xl">{totalContributions}</p>
+                <p className="text-5xl font-medium">{totalContributions}</p>
             </div>
-            <div className="col-span-1 p-6">
+            <div className="col-span-1 p-6 bg-white rounded-md text-sky-950">
                 <h2 className={cn("uppercase text-xl", generateFontByLocale(locale))}>
                     {t("pending")}
                 </h2>
-                <p className="text-5xl">{totalPending}</p>
+                <p className="text-5xl font-medium">{totalPending}</p>
             </div>
         </div>
     )
