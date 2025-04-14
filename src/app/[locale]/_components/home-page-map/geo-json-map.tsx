e864cba31dbb998bf3289cfff8bbfde9b93c2d95
ratigumashvili <ratigumashvili@gmail.com>
2025-04-14 16:13:04 +0400

    cleanup

--- src/app/[locale]/_components/home-page-map/geo-json-map.tsx
+++ src/app/[locale]/_components/home-page-map/geo-json-map.tsx
@@ -7,9 +7,11 @@ import * as turf from "@turf/turf";
 import L, { PathOptions } from "leaflet";
 import { Feature, Geometry } from "geojson";
 import { useTranslations } from "next-intl";
+import { DownloadIcon } from "lucide-react";
 
 import data from "@/app/[locale]/_data/coords.json";
-import { DownloadIcon } from "lucide-react";
+
+import { BASE_API_URL } from "@/lib/utils";
 
 type GeoJSONFeature = Feature<Geometry, { NAME_2?: string; name?: string }>;
 type GeoJSONData = { type: "FeatureCollection"; features: GeoJSONFeature[] };
@@ -185,12 +187,12 @@ export default function GeoJsonMap({ speciesCoordinates }: { speciesCoordinates:
             )}
             {markers}
             
-            <Link href={`http://localhost:1337/api/export-coordinates/species`}
-                className="absolute top-4 right-4 z-[1000] bg-white border p-2 rounded shadow !text-black"
+            <Link href={`${BASE_API_URL}/export-coordinates/species`}
+                className="absolute top-4 right-4 z-[1000] bg-white border p-[7px] rounded shadow !text-black"
             >
                 <DownloadIcon className="w-4 h-4" />
             </Link>
-            
+
         </MapContainer>
     );
 }
