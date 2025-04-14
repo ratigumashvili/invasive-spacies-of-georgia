648f09419c468e8e25c2f52dcc3fce7f1cf0d851
ratigumashvili <ratigumashvili@gmail.com>
2025-04-14 21:05:42 +0400

    cleanup

--- src/app/[locale]/_components/drop-down-actions.tsx
+++ src/app/[locale]/_components/drop-down-actions.tsx
@@ -1,28 +1,31 @@
 "use client"
 
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+import { EllipsisIcon } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+
 import { useFullUrl } from "@/hooks/use-full-url"
 import { exportDataAsCSV } from "@/lib/utils"
+
 import { DistributionDownload, SpecieDownload } from "@/types/specie-response"
-import { SettingsIcon } from "lucide-react"
-import { toast } from "sonner"
 
 export function DropDownAction({
-    specieData, 
+    specieData,
     distribution
 }: {
-    specieData: SpecieDownload, 
+    specieData: SpecieDownload,
     distribution: DistributionDownload[]
 }) {
 
     const url = useFullUrl()
 
-    function copyToClipboard () {
+    function copyToClipboard() {
         navigator.clipboard.writeText(url)
         toast.success("Link copied to clipboard")
     }
@@ -30,15 +33,22 @@ export function DropDownAction({
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <button className="cursor-pointer p-2 focus:outline-0 print:hidden">
-                    <SettingsIcon className="w-5 h-5" />
-                </button>
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="cursor-pointer px-4 print:hidden shadow-none focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
+                >
+
+                    <EllipsisIcon className="w-7 h-7" />
+                </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="bg-white z-10 min-w-[200px] rounded-none" align="end">
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={copyToClipboard}>
                     Copy url
                 </DropdownMenuItem>
-                <DropdownMenuItem className="text-base cursor-pointer rounded-none">Print page</DropdownMenuItem>
+                <DropdownMenuItem className="text-base cursor-pointer rounded-none">
+                    Print page
+                </DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={() => exportDataAsCSV(specieData)}>
                     Download specie data
                 </DropdownMenuItem>
