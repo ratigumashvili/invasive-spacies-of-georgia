e35d5b8dedd2c6d897518a1e57f184713642490e
ratigumashvili <ratigumashvili@gmail.com>
2025-04-14 21:09:05 +0400

    added translations to actions

--- src/app/[locale]/_components/drop-down-actions.tsx
+++ src/app/[locale]/_components/drop-down-actions.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { toast } from "sonner"
-import { Button } from "@/components/ui/button"
+import { useTranslations } from "next-intl"
 import { EllipsisIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -23,6 +25,8 @@ export function DropDownAction({
     distribution: DistributionDownload[]
 }) {
 
+    const t = useTranslations("Common")
+
     const url = useFullUrl()
 
     function copyToClipboard() {
@@ -38,22 +42,21 @@ export function DropDownAction({
                     size="icon"
                     className="cursor-pointer px-4 print:hidden shadow-none focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
                 >
-
                     <EllipsisIcon className="w-7 h-7" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="bg-white z-10 min-w-[200px] rounded-none" align="end">
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={copyToClipboard}>
-                    Copy url
+                    {t("copyUrl")}
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none">
-                    Print page
+                    {t("printPage")}
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={() => exportDataAsCSV(specieData)}>
-                    Download specie data
+                    {t("speciesData")}
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-base cursor-pointer rounded-none" onClick={() => exportDataAsCSV(distribution)}>
-                    Download distribution data
+                    {t("distributionData")}
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
